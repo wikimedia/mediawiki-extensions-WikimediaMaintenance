2ebcd0230ee312cc3865f787f1fa43c9bc8e8a8c
Chad Horohoe <demon@users.mediawiki.org>
2011-09-08 00:44:22 +0000

    Followup r96529: forgot svn add

---
+++ WikimediaMaintenance.php
@@ -0,0 +1,74 @@
+<?php
+/* 
+ * Entry point for WikimediaMaintenance scripts :)
+ */
+
+// Detect $IP
+$IP = getenv( 'MW_INSTALL_PATH' );
+if ( $IP === false ) {
+	$IP = dirname( __FILE__ ) . '/../..';
+}
+
+// Require base maintenance class
+require( "$IP/maintenance/Maintenance.php" );
+
+// Some scripts need the WMFSite class, so include that too
+require( dirname( __FILE__ ) . '/WMFSite.php' );
+
+/**
+ * Wikimedia-specific maintenance classes should extend this. This class will
+ * override some maintenance setup process to be wmf-specific.
+ */
+abstract class WikimediaMaintenance extends Maintenance {
+	/**
+	 * Override the core loadSettings.
+	 */
+	public function loadSettings() {
+		global $IP, $wgNoDBParam, $wgUseNormalUser, $wgConf, $site, $lang;
+
+		if ( empty( $wgNoDBParam ) ) {
+			# Check if we were passed a db name
+			if ( isset( $this->mOptions['wiki'] ) ) {
+				$db = $this->mOptions['wiki'];
+			} else {
+				$db = array_shift( $this->mArgs );
+			}
+			list( $site, $lang ) = $wgConf->siteFromDB( $db );
+
+			# If not, work out the language and site the old way
+			if ( is_null( $site ) || is_null( $lang ) ) {
+				if ( !$db ) {
+					$lang = 'aa';
+				} else {
+					$lang = $db;
+				}
+				if ( isset( $this->mArgs[0] ) ) {
+					$site = array_shift( $this->mArgs );
+				} else {
+					$site = 'wikipedia';
+				}
+			}
+		} else {
+			$lang = 'aa';
+			$site = 'wikipedia';
+		}
+
+		# This is for the IRC scripts, which now run as the apache user
+		# The apache user doesn't have access to the wikiadmin_pass command
+		if ( $_ENV['USER'] == 'apache' ) {
+		# if ( posix_geteuid() == 48 ) {
+			$wgUseNormalUser = true;
+		}
+
+		putenv( 'wikilang=' . $lang );
+
+		ini_set( 'include_path', ".:$IP:$IP/includes:$IP/languages:$IP/maintenance" );
+
+		if ( $lang == 'test' && $site == 'wikipedia' ) {
+			if ( !defined( 'TESTWIKI' ) ) {
+				define( 'TESTWIKI', 1 );
+			}
+		}
+		return '../wmf-config/CommonSettings.php';
+	}
+}
