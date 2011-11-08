bdafbb5f6809d95180eb4613d5c88008f94ce906
Roan Kattouw <catrope@users.mediawiki.org>
2011-11-08 11:05:40 +0000

    (bug 29591) Special-case sourceswiki in the interwiki map

--- dumpInterwiki.php
+++ dumpInterwiki.php
@@ -89,6 +89,11 @@ class DumpInterwiki extends WikimediaMaintenance {
 			'wikiversity' => new WMFSite( 'wikiversity', 'v', 'wikiversity.org' ),
 		);
 
+		# Site overrides for wikis whose DB names end in 'wiki' but that really belong to another site
+		$siteOverrides = array(
+			'sourceswiki' => 'wikisource',
+		);
+
 		# Extra interwiki links that can't be in the intermap for some reason
 		$extraLinks = array(
 			array( 'm', $this->urlprotocol . '//meta.wikimedia.org/wiki/$1', 1 ),
@@ -176,13 +181,18 @@ class DumpInterwiki extends WikimediaMaintenance {
 			} else {
 				# Find out which site this DB belongs to
 				$site = false;
-				foreach ( $sites as $candidateSite ) {
-					$suffix = $candidateSite->suffix;
-					if ( preg_match( "/(.*)$suffix$/", $db, $matches ) ) {
-						$site = $candidateSite;
-						break;
+				if ( isset( $siteOverrides[$db] ) ) {
+					$site = $sites[$siteOverrides[$db]];
+				} else {
+					foreach ( $sites as $candidateSite ) {
+						$suffix = $candidateSite->suffix;
+						if ( preg_match( "/(.*)$suffix$/", $db, $matches ) ) {
+							$site = $candidateSite;
+							break;
+						}
 					}
 				}
+
 				$this->makeLink( array( 'iw_prefix' => $db, 'iw_url' => $site->suffix ), "__sites" );
 				if ( !$site ) {
 					$this->error( "Invalid database $db\n" );
