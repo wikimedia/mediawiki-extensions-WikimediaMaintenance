18873395273043eac998ec9efb5200d3470849ba
Antoine Musso <hashar@free.fr>
2012-06-12 15:59:58 +0200

    jobs-loop: update comment + basic man page
    This give a bit of documentation about jobs-loop.sh ala man page. It
    might help newcomers figuring it out, or at least give some clue to
    people unfamilar with the job queue and trying to fix it on the cluster.
    
    This patch describe jobs-loop.sh as it was intended. Aka we had a way to
    pass a job type as an argument to have the loop to only proceed that
    kind of type. Some refactoring dropped support for it though, that need
    to be fixed in a follow up commit.
    
    No functional change in this patch.
    
    Change-Id: I1633f8d8786b50734e5d1f64985d0cc648415345

--- jobs-loop.sh
+++ jobs-loop.sh
@@ -1,7 +1,39 @@
 #!/bin/bash
+#
+# NAME
+# jobs-loop.sh -- Continuously process a MediaWiki jobqueue
+#
+# SYNOPSIS
+# jobs-loop.sh [job_type]
+#
+# DESCRIPTION
+# jobs-loop.sh is an infinite "while loop" used to call MediaWiki runJobs.php
+# and eventually attempt to process any job enqueued. MediaWiki jobs are split
+# into several types, by default jobs-loop.sh will:
+#  - first attempt to run the internally priorized jobs (see `types` variable
+#    in script).
+#  - proceed any default jobs
+#
+# MediaWiki configuration variable $wgJobTypesExcludedFromDefaultQueue is used
+# to exclude some types from the default processing. Those excluded job types
+# could be processed on dedicated boxes by running jobs-loop.sh using the
+# job_type parameter.
+#
+# You will probably want to run this script under your webserver username.
+#
+# Example:
+# // Process job queues:
+# jobs-loop.sh
+#
+# // Process jobs of type `webVideoTranscode`
+# jobs-loop.sh webVideoTranscode
+#
 
+# Limit virtual memory
 ulimit -v 400000
 
+# When killed, make sure we are also getting ride of the child jobs
+# we have spawned.
 trap 'kill %-; exit' SIGTERM
 [ ! -z "$1" ] && {
 	echo "starting type-specific job runner: $1"
@@ -13,6 +45,7 @@ types="sendMail enotifNotify uploadFromUrl fixDoubleRedirect MoodBarHTMLMailerJo
 
 cd `readlink -f /usr/local/apache/common/multiversion`
 while [ 1 ];do
+
 	# Do the prioritised types
 	moreprio=y
 	while [ -n "$moreprio" ] ; do
