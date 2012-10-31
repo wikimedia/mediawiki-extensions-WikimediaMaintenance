fd5aa07defe7721e9d9f6892121b011929f353ed
Aaron Schulz <aschulz@wikimedia.org>
2012-10-31 12:18:05 -0700

    Added a script to clean up file 404s.
    Change-Id: I95c4a54570c6205e3067865aae363d705e806990

---
+++ fixBug39615.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * @defgroup Wikimedia Wikimedia
+ */
+
+/**
+ * Recover evicted files from bug 39615.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Maintenance
+ * @ingroup Wikimedia
+ */
+require_once( __DIR__ . '/WikimediaMaintenance.php' );
+
+class FixBug39615 extends WikimediaMaintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->addOption( 'move', "Actually move the files", false, false );
+		$this->mDescription = "Fix files that were affected by bug 39615 and still broken";
+		$this->setBatchSize( 100 );
+	}
+
+	public function execute() {
+		$name = ''; // page on img_name
+		$repo = RepoGroup::singleton()->getLocalRepo();
+
+		$dbr = wfGetDB( DB_SLAVE );
+		$cutoff = $dbr->addQuotes( $dbr->timestamp( time() - 86400*60 ) ); // 2 months
+		while ( true ) {
+			$this->output( "Doing next batch from '$name'.\n" );
+			$res = $dbr->select( 'image', '*',
+				array( "img_name > {$dbr->addQuotes( $name )}" ),
+				__METHOD__,
+				array( 'ORDER BY' => 'img_name ASC', 'LIMIT' => $this->mBatchSize )
+			);
+			if ( !$res->numRows() ) {
+				break; // done
+			}
+			foreach ( $res as $row ) {
+				$name = $row->img_name;
+				$file = LocalFile::newFromRow( $row, $repo );
+				$file->lock();
+				if ( !$repo->fileExists( $file->getPath() ) ) { // 404
+					$jpath = $dbr->selectField( 'filejournal',
+						'fj_path',
+						array( 'fj_new_sha1' => $file->getSha1(), "fj_timestamp > {$cutoff}" ),
+						__METHOD__,
+						array( 'ORDER BY' => 'fj_id DESC' )
+					);
+					if ( $jpath === false || strpos( $jpath, "!" ) === false ) {
+						continue; // no entry or not evicted to archive name ("<timestamp>!<name>")
+					}
+					$path = preg_replace( # fj_path is under the "master" backend
+						'!^mwstore://[^/]+!/', $repo->getBackend()->getRootStoragePath(), $jpath
+					);
+					if ( $repo->getFileSha1( $path ) === $file->getSha1() ) {
+						# File was evicted to an archive name
+						if ( $this->hasOption( 'move' ) ) {
+							$status = $this->repo()->getBackend()->move( array(
+								'src' => $path, 'dst' => $file->getPath()
+							) );
+							if ( !$status->isOK() ) {
+								print_r( $status->getErrorsArray() );
+							} else {
+								$this->output( "Moved $path to {$file->getPath()}.\n" );
+							}
+						} else {
+							$this->output( "Should move $path to {$file->getPath()}.\n" );
+						}
+					}
+				}
+				$file->unlock();
+			}
+		}
+	}
+}
+
+$maintClass = "FixBug39615";
+require_once( RUN_MAINTENANCE_IF_MAIN );
