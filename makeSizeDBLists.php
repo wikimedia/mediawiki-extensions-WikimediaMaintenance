0fa0b55538fc1e9645198c3aba85149f7c4826a2
Reedy <reedy@wikimedia.org>
2012-11-16 02:16:39 +0000

    Bug 39667 - Divide wikis into database lists by approximate size for performance engineering
    Basic script to dump list of wiki sizes based on ss_total_pages
    
    Change-Id: Ib00444f03ef65a3a01ba90e1377d4962b06ac7c7

---
+++ makeSizeDBLists.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Builds database lists of wikis based on size
+ */
+
+require_once( __DIR__ .'/WikimediaMaintenance.php' );
+
+class MakeSizeDBLists extends WikimediaMaintenance {
+
+	const DB_SMALL = 10000;
+	const DB_MEDIUM = 1000000;
+
+	function __construct() {
+		parent::__construct();
+		$this->mDescription = 'Builds database lists of wikis based on size';
+	}
+
+	function execute() {
+		global $wgConf;
+		$small = array();
+		$medium = array();
+		$large = array();
+		foreach ( $wgConf->getLocalDatabases() as $wiki ) {
+			$lb = wfGetLB( $wiki );
+			$db = $lb->getConnection( DB_MASTER, array(), $wiki );
+			$count = intval( $db->selectField( 'site_stats', 'ss_total_pages', '', __METHOD__ ) );
+			if ( $count < self::DB_SMALL ) {
+				$small[] = $wiki;
+			} elseif( $count < self::DB_MEDIUM ) {
+				$medium[] = $wiki;
+			} else {
+				$large[] = $wiki;
+			}
+			$lb->reuseConnection( $db );
+		}
+
+		$this->output( 'Small wiki count: ' . count( $small ) . "\n" );
+		$this->writeFile( '/home/wikipedia/common/small.dblist', $small );
+		$this->output( 'Medium wiki count: ' . count( $medium ) . "\n" );
+		$this->writeFile( '/home/wikipedia/common/medium.dblist', $medium );
+		$this->output( 'Large wiki count: ' . count( $large ) . "\n" );
+		$this->writeFile( '/home/wikipedia/common/large.dblist', $large );
+	}
+
+	/**
+	 * @param $filename string
+	 * @param $items array
+	 */
+	private function writeFile( $filename, $items ) {
+		$file = fopen( $filename, 'w' );
+		fwrite( $file, implode( "\n", $items ) );
+		fclose( $file );
+	}
+}
+
+$maintClass = 'MakeSizeDBLists';
+require_once( DO_MAINTENANCE );
