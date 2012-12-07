6de1f15ec0a955148899950b4874821a0805cf38
Reedy <reedy@wikimedia.org>
2012-12-07 15:28:11 +0000

    Only output job queue lengths for wikis that are non zero by default (add option to output them)
    Change-Id: Id21054f53c9fa66ff76894dcf5ebbf0d04fd349c

--- getJobQueueLengths.php
+++ getJobQueueLengths.php
@@ -10,17 +10,21 @@ class GetJobQueueLengths extends WikimediaMaintenance {
 	function __construct() {
 		parent::__construct();
 		$this->mDescription = 'Get the length of the job queue on all wikis in $wgConf';
+		$this->addOption( 'showzero', 'Whether to output wikis with 0 jobs', false, false, false );
 	}
 
 	function execute() {
 		global $wgConf;
+		$outputZero = $this->getOption( 'showzero', false );
 		$total = 0;
 		foreach ( $wgConf->getLocalDatabases() as $wiki ) {
 			$lb = wfGetLB( $wiki );
 			$db = $lb->getConnection( DB_MASTER, array(), $wiki );
 			$count = intval( $db->selectField( 'job', 'COUNT(*)', '', __METHOD__ ) );
-			$this->output( "$wiki $count\n" );
-			$total += $count;
+			if ( $outputZero || $count > 0 ) {
+				$this->output( "$wiki $count\n" );
+				$total += $count;
+			}
 			$lb->reuseConnection( $db );
 		}
 		$this->output( "Total $total" );
