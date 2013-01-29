833de435ca25f136cc37113f95f9fbd5e5dc74c0
Reedy <reedy@wikimedia.org>
2013-01-29 22:15:01 +0000

    Change script for creating translate tables to more generic
    Now supports WikiLove and MoodBar
    
    Seems git doesn't want to see it as a rename either. Oh well.
    
    Change-Id: I2cc05aa759af5622f45e3bcc25189033707ab70c

---
+++ createExtensionTables.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once( __DIR__ . '/WikimediaMaintenance.php' );
+
+/**
+ * Creates the necessary tables to install various extensions on a WMF wiki
+ */
+class CreateExtensionTables extends WikimediaMaintenance {
+	function __construct() {
+		parent::__construct();
+		$this->addArg( 'extension', 'Which extension to install' );
+	}
+
+	function execute() {
+		global $IP;
+		$dbw = $this->getDB( DB_MASTER );
+		$extension = $this->getArg();
+
+		$files = array();
+		$path = '';
+
+		switch ( strtolower( $extension ) ) {
+			case 'moodbar':
+				$files = array(
+					'MoodBar.sql',
+					'moodbar_feedback_response.sql',
+				);
+				$path = "$IP/extensions/MoodBar/sql";
+				break;
+
+			case 'translate':
+				$files = array(
+					'revtag.sql',
+					'translate_groupstats.sql',
+					'translate_metadata.sql',
+					'translate_sections.sql',
+					'translate_groupreviews.sql',
+					'translate_messageindex.sql',
+					'translate_reviews.sql',
+				);
+				$path = "$IP/extensions/Translate/sql";
+				break;
+
+			case 'wikilove':
+				$files = array(
+					'WikiLoveImageLog.sql',
+					'WikiLoveLog.sql',
+				);
+				$path = "$IP/extensions/WikiLove/patches";
+				break;
+
+			default:
+				$this->error( "This script is not configured to create tables for $extension\n", 1 );
+		}
+
+		$this->output( "Creating $extension tables..." );
+		foreach( $files as $file ) {
+			$dbw->sourceFile( "$path/$file" );
+		}
+		$this->output( "done!\n" );
+	}
+}
+
+$maintClass = 'CreateExtensionTables';
+require_once( DO_MAINTENANCE );
