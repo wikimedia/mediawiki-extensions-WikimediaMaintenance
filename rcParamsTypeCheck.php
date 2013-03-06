9c1d4c67280fb1cad0794e94754cd0cd300c6da5
Reedy <reedy@wikimedia.org>
2013-03-06 20:49:52 +0000

    Stashing script for checking rc_params type
    Change-Id: I0ddde137d5d63821eb2053b849416c60cff5f58a

---
+++ rcParamsTypeCheck.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * Get the length of the job queue on all wikis in $wgConf
+ */
+
+require_once( __DIR__ .'/WikimediaMaintenance.php' );
+
+class GetJobQueueLengths extends WikimediaMaintenance {
+	function __construct() {
+		parent::__construct();
+		$this->mDescription = '';
+	}
+
+	function execute() {
+		global $wgConf;
+		$count = 0;
+		foreach ( $wgConf->getLocalDatabases() as $wiki ) {
+			$lb = wfGetLB( $wiki );
+			$db = $lb->getConnection( DB_MASTER, array(), $wiki );
+			$field = $db->fieldInfo( 'recentchanges', 'rc_params' );
+			if ( $field->type() !== "blob" ) {
+				echo $wiki . "\n";
+				$count++;
+			}
+			$lb->reuseConnection( $db );
+		}
+		$this->output( $count . "\n" );
+	}
+}
+
+$maintClass = 'GetJobQueueLengths';
+require_once( DO_MAINTENANCE );
