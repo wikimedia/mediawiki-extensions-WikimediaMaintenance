a30fc031b573ec3310a17ce466f956531fc53a36
Aaron Schulz <aschulz@wikimedia.org>
2014-04-30 21:05:14 -0700

    Added --grouponly for getting per wiki/type breakdown in JSON
    Change-Id: I0b502aa6b9353aa62feecacdd915ec9a2799d819

--- getJobQueueLengths.php
+++ getJobQueueLengths.php
@@ -12,30 +12,37 @@ class GetJobQueueLengths extends WikimediaMaintenance {
 		$this->mDescription = 'Get the length of the job queue on all wikis in $wgConf';
 		$this->addOption( 'totalonly', 'Whether to only output the total number of jobs' );
 		$this->addOption( 'nototal', "Don't print the total number of jobs" );
+		$this->addOption( 'grouponly', "Show a per-wiki/per-type count map in JSON" );
 	}
 
 	function execute() {
 		$totalOnly = $this->hasOption( 'totalonly' );
 
-		$total = 0;
 		$pendingDBs = JobQueueAggregator::singleton()->getAllReadyWikiQueues();
-		$sizeByWiki = array();
+		$sizeByWiki = array(); // (wiki => type => count) map
 		foreach ( $pendingDBs as $type => $wikis ) {
 			foreach ( $wikis as $wiki ) {
-				$sizeByWiki[$wiki] = isset( $sizeByWiki[$wiki] ) ? $sizeByWiki[$wiki] : 0;
-				$sizeByWiki[$wiki] += JobQueueGroup::singleton( $wiki )->get( $type )->getSize();
+				$sizeByWiki[$wiki][$type] =
+					JobQueueGroup::singleton( $wiki )->get( $type )->getSize();
 			}
 		}
-		foreach ( $sizeByWiki as $wiki => $count ) {
-			if ( $count > 0 ) {
-				if ( !$totalOnly ) {
-					$this->output( "$wiki $count\n" );
+
+		if ( $this->hasOption( 'grouponly' ) ) {
+			$this->output( FormatJSON::encode( $sizeByWiki, true ) . "\n" );
+		} else {
+			$total = 0;
+			foreach ( $sizeByWiki as $wiki => $counts ) {
+				$count = array_sum( $counts );
+				if ( $count > 0 ) {
+					if ( !$totalOnly ) {
+						$this->output( "$wiki $count\n" );
+					}
+					$total += $count;
 				}
-				$total += $count;
 			}
-		}
-		if ( !$this->hasOption( 'nototal' ) ) {
-			$this->output( "Total $total\n" );
+			if ( !$this->hasOption( 'nototal' ) ) {
+				$this->output( "Total $total\n" );
+			}
 		}
 	}
 }
