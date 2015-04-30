1b4a6219eee93ffe7fbd1d8ba297c484bafa5467
Alex Monk <krenair@gmail.com>
2015-04-30 20:24:57 +0100

    Stop creating Oversight tables.
    Change-Id: I1024f8867eb9a02e8c2c03fb8a4c733272cb2b4a

--- addWiki.php
+++ addWiki.php
@@ -93,7 +93,6 @@ class AddWiki extends WikimediaMaintenance {
 		$dbw->sourceFile( "$IP/extensions/CheckUser/cu_changes.sql" );
 		$dbw->sourceFile( "$IP/extensions/CheckUser/cu_log.sql" );
 		$dbw->sourceFile( "$IP/extensions/TitleKey/titlekey.sql" );
-		$dbw->sourceFile( "$IP/extensions/Oversight/hidden.sql" );
 		$dbw->sourceFile( "$IP/extensions/GlobalBlocking/globalblocking.sql" );
 		$dbw->sourceFile( "$IP/extensions/AbuseFilter/abusefilter.tables.sql" );
 		$dbw->sourceFile( "$IP/extensions/UserDailyContribs/patches/UserDailyContribs.sql" );
