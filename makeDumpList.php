c2c8e5fd54357ea9fc1c2063fd9835af48db577e
Tim Starling <tstarling@wikimedia.org>
2016-03-04 10:31:55 +1100

    Make a title list for a partial wiki dump
    To support visual diffing for depurate validation etc.
    
    Change-Id: Idf3a17ee96b89d00f1968a2f1f451505058e2f40

---
+++ makeDumpList.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once( __DIR__ . '/WikimediaMaintenance.php' );
+
+class MakeDumpList extends WikimediaMaintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->mDescription = 'From a list of page titles, generate a list of titles needed to render those pages, including templates and the pages themselves.';
+	}
+
+	private $templates = [];
+
+	public function execute() {
+		$linkBatch = new LinkBatch;
+		$batchSize = 0;
+		while ( false !== ( $line = fgets( STDIN ) ) ) {
+			$line = trim( $line );
+			$title = Title::newFromText( $line );
+			if ( !$title ) {
+				fwrite( STDERR, "Invalid title: $line\n" );
+				continue;
+			}
+			print $title->getPrefixedDBkey() . "\n";
+
+			$linkBatch->addObj( $title );
+			$batchSize++;
+			if ( $batchSize > 100 ) {
+				$this->doBatch( $linkBatch );
+				$linkBatch = new LinkBatch;
+				$batchSize = 0;
+			}
+		}
+		if ( $batchSize ) {
+			$this->doBatch( $linkBatch );
+		}
+		foreach ( $this->templates as $template => $unused ) {
+			print "$template\n";
+		}
+	}
+
+	public function doBatch( $linkBatch ) {
+		$dbr = wfGetDB( DB_SLAVE );
+		$conds = [
+			$linkBatch->constructSet( 'page', $dbr ),
+			'page_id=tl_from'
+		];
+		$res = $dbr->select(
+			['page', 'templatelinks'],
+			['tl_namespace', 'tl_title'],
+			$conds,
+			__METHOD__ );
+		foreach ( $res as $row ) {
+			$title = Title::makeTitle( $row->tl_namespace, $row->tl_title );
+			$this->templates[$title->getPrefixedDBkey()] = true;
+		}
+	}
+}
+
+$maintClass = "MakeDumpList";
+require_once( RUN_MAINTENANCE_IF_MAIN );
