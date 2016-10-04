7829cae227c405e25fe562e66030ff7cef10009b
Timo Tijhof <krinklemail@gmail.com>
2016-10-04 20:32:39 +0100

    Implement purgeUrls.php
    Simplified version of MediaWiki core maintenance/purgeList.php.
    
    The difference is that it supports a --cluster option. Right now
    mainly to support easy purging of static urls served by cache_misc
    which get cached for 1 hour by default.
    
    Test plan:
    * Store in home directory on terbium.
    * Manually substitute WikimediaMaintenance.php require.
    * Run the following:
      > echo 'https://noc.wikimedia.org/db.php' | mwscript ../../../home/krinkle/purgeUrls.php --wiki=aawiki --cluster=cache_misc
    * Verify that its Age reset to 0.
    
    TODO: Add to MWScript wikiless list so that --wiki isn't required.
    
    Change-Id: Id431cd3aeddb40b27fb9e7256b549370e6b536d3

---
+++ purgeUrls.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Maintenance
+ */
+
+require_once __DIR__ . '/WikimediaMaintenance.php';
+
+class WikimediaPurgeUrls extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( 'Send purge requests to Varnish for listed urls' );
+		$this->addOption( 'cluster', 'The Varnish cluster to be contacted (default: cache_text).', false, true );
+		$this->addOption( 'verbose', 'Show more output', false, false, 'v' );
+		$this->setBatchSize( 100 );
+	}
+
+	public function execute() {
+		global $wgSquidServers, $wgHTCPRouting;
+
+		// Ensure direct purging is disabled
+		$wgSquidServers = [];
+
+		// Select cluster
+		$cluster = $this->getOption( 'cluster', 'cache_text' );
+		switch ( $cluster ) {
+		case 'cache_text':
+			// Use value from wmf-config (239.128.0.112, 4827)
+			break;
+		case 'cache_misc':
+			// FIXME: Hardcoded https://github.com/wikimedia/operations-puppet/commit/71e9017
+			$wgHTCPRouting = [ '' => [ 'host' => '239.128.0.115', 'port' => 4827 ] ];
+			break;
+		default:
+			$this->error( 'Invalid --cluster value.' );
+			return;
+		}
+
+		$urls = [];
+		$stdin = $this->getStdin();
+		while ( !feof( $stdin ) ) {
+			$url = trim( fgets( $stdin ) );
+			if ( preg_match( '%^https?://%', $url ) ) {
+				$urls[] = $url;
+			} else if ( $url !== '' ) {
+				$this->output( "Invalid url (skipped): $url\n" );
+			}
+		}
+		$this->output( "Purging " . count( $urls ) . " urls\n" );
+		$this->doPurge( $urls );
+		$this->output( "Done!\n" );
+	}
+
+	private function doPurge( array $urls ) {
+		$chunks = array_chunk( $urls, $this->mBatchSize );
+		foreach ( $chunks as $chunk ) {
+			if ( $this->hasOption( 'verbose' ) ) {
+				$this->output( implode( "\n", $urls ) . "\n" );
+			}
+			// Use ::purge() instead of doUpdate() to bypass jobqueue rebound purge
+			CdnCacheUpdate::purge( $chunk );
+		}
+	}
+}
+
+$maintClass = 'WikimediaPurgeUrls';
+require_once RUN_MAINTENANCE_IF_MAIN;
