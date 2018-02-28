aa3b968108cb0175c244c085f39ab047b764bf62
Chad Horohoe <chadh@wikimedia.org>
2018-02-28 11:26:11 -0800

    Remove flaggedrevs-periodic-update.sh
    Handled by puppet which just does this directly without a shell script
    
    Change-Id: I96a6b2c1ae9a952ae61445627bcfa4af9e5c42bb

--- flaggedrevs-periodic-update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-FILE="/srv/mediawiki/dblists/flaggedrevs.dblist"
-
-for db in `<$FILE`;do
-	echo $db
-	php -n /srv/mediawiki/multiversion/MWScript.php extensions/FlaggedRevs/maintenance/updateStats.php $db
-done
