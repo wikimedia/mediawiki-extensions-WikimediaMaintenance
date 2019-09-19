a611c4a6388018a569bc0468664bf2bf4c0345b0
Petr Pchelko <ppchelko@wikimedia.org>
2019-09-19 14:06:09 -0700

    Remove obsolete scripts that use rev_text_id.
    - purgeStaleMemcachedText.php is a script that removes memcached entries
    before a hardcoded date in December 2010. Was dropped from core by
    I2ae469063eba502e662f80c38d5816c371550352
    - bug-53687 scripts seem to be used to fix T55687 which is resolved since
    2013, so I believe the script is unlikely to be used again.
    
    Bug: T233355
    Change-Id: Ie20cd1e9103ab79a4f3830afb70d6a7c6249b9db

--- bug-53687/find-orphans.sql
+++
@@ -1,2 +0,0 @@
-select up_page,up_timestamp,log_namespace,log_title,rev_id,ar_rev_id is not null as ar_rev_match,rev_text_id=ar_text_id as ar_text_match from revision left join archive on ar_rev_id=rev_id ,updates left join logging on up_timestamp=log_timestamp  where log_action='delete' and rev_page=up_page and up_action='delete';
-
