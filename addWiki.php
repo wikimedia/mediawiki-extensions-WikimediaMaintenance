98790c126e23fdc461dee2209bdf3c935c129c6b
Reedy <reedy@wikimedia.org>
2020-08-22 22:00:57 +0100

    Update Babel sql path
    Change-Id: I0272a3abaae9377dfb0399056018e86c0150561b
    Depends-On: I2a67561fc5d1dc5c7463a4ab389be9271300d347

--- addWiki.php
+++ addWiki.php
@@ -263,7 +263,7 @@ class AddWiki extends Maintenance {
 			[ $this, 'noExecuteCommands' ]
 		);
 		$dbw->sourceFile( "$IP/extensions/AntiSpoof/sql/patch-antispoof.mysql.sql" );
-		$dbw->sourceFile( "$IP/extensions/Babel/babel.sql" );
+		$dbw->sourceFile( "$IP/extensions/Babel/sql/tables-generated.sql" );
 		$dbw->sourceFile( "$IP/extensions/CheckUser/cu_changes.sql" );
 		$dbw->sourceFile( "$IP/extensions/CheckUser/cu_log.sql" );
 		$dbw->sourceFile( "$IP/extensions/GlobalBlocking/sql/global_block_whitelist.sql" );
