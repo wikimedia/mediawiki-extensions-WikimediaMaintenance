de66c74cb98dbd8186485b54fc592875708c8f6f
Reedy <reedy@wikimedia.org>
2020-08-22 22:23:56 +0100

    Update TimedMediaHandler sql path
    Change-Id: I7ea40828335d656bbb4c53b3764061a49e54fb4f
    Depends-On: I77dd66e77a5149a7cfe7d9b50161f0aa9b6cecaa

--- addWiki.php
+++ addWiki.php
@@ -269,7 +269,7 @@ class AddWiki extends Maintenance {
 		$dbw->sourceFile( "$IP/extensions/GlobalBlocking/sql/global_block_whitelist.sql" );
 		$dbw->sourceFile( "$IP/extensions/AbuseFilter/abusefilter.tables.sql" );
 		$dbw->sourceFile( "$IP/extensions/Math/db/mathoid.mysql.sql" );
-		$dbw->sourceFile( "$IP/extensions/TimedMediaHandler/sql/TimedMediaHandler.sql" );
+		$dbw->sourceFile( "$IP/extensions/TimedMediaHandler/sql/tables-generated.sql" );
 		// Not actually enabled everywhere, but this is easier
 		$dbw->sourceFile( "$IP/extensions/GeoData/sql/externally-backed.sql" );
 		$dbw->sourceFile( "$IP/extensions/BetaFeatures/sql/create_counts.sql" );
