e3837c2dd4cfdc6ca5b711aacdf7b5c8a744e3bf
Jcrespo <jcrespo@wikimedia.org>
2021-09-09 20:02:07 +0200

    maintenance: Add email search script
    This script takes as input a users email and produces a json blob
    indicating the username, email and email verification date (if present)
    for all users matching the email address.
    
    The intention is to call this script remotely using a cookbook and have
    that cookbook process the json response in combination with data from
    gsuite and possibly namely.
    
    Usage:
    
    $ getUserByEmail.php --email='user@example.org'
    [
      {
        "username": "My_user",
        "email": "user@example.org",
        "email_authenticated_date": "20151231165901",
      }
    ]
    
    Bug: T259746
    Change-Id: If8f7a0da09b51af3bb30396d7d7f126dd44ad501

---
+++ getUsersByEmail.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ * @ingroup Maintenance
+ * @author <jbond at wikimedia dot org>
+ * @license GPL-2.0-or-later
+ */
+
+require_once __DIR__ . '/WikimediaMaintenance.php';
+
+/**
+ * Maintenance script to gather data associated with a specific email address
+ *
+ * This script will return a json blob containing the usernames and
+ * authenticated date for all users matching a specific email address.
+ *
+ * The authenticated date indicates the date the account was
+ * authenticated/verified via email.  If this value is empty/null it indicates
+ * the user never completed the verification process
+ *
+ * @ingroup Maintenance
+ */
+class GetUsersByEmail extends Maintenance {
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( 'Return a json blob containing the usernames and authenticated date '
+			. 'for all users matching a specific email address' );
+		$this->addOption( 'email', 'E-mail address', true, true );
+	}
+
+	public function execute() {
+		$email = trim( $this->getOption( 'email' ) );
+		$dbr = $this->getDB( DB_REPLICA );
+		$res = $dbr->select(
+			'user',
+			[ 'user_name', 'user_email', 'user_email_authenticated' ],
+			[ 'user_email' => $email ],
+			__METHOD__
+		);
+		if ( $res->numRows() === 0 ) {
+			$this->fatalError( $email . " was not found on the wiki" );
+		}
+
+		$json_data = [];
+		foreach ( $res as $row ) {
+			$json_data[] = [
+				"username" => $row->user_name,
+				"email" => $row->user_email,
+				"email_authenticated_date" => $row->user_email_authenticated,
+			];
+		}
+
+		$this->output( json_encode( $json_data, JSON_PRETTY_PRINT ) . '\n' );
+	}
+}
+
+$maintClass = GetUsersByEmail::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
