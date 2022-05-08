8903ad3e9ca8bbe606253c584456dbbc7ca101c0
Umherirrender <umherirrender_de.wp@web.de>
2022-05-08 20:38:17 +0200

    addWiki.php: Use abstract schema sql file for AntiSpoof
    Depends-On: I19c17396d6383b16a568bdb4a041aab03b910f3e
    Change-Id: I140e173d3bc47c6dab2ad1225bcda501d0066999

--- addWiki.php
+++ addWiki.php
@@ -284,7 +284,7 @@ class AddWiki extends Maintenance {
 			__METHOD__,
 			[ $this, 'noExecuteCommands' ]
 		);
-		$dbw->sourceFile( "$IP/extensions/AntiSpoof/sql/patch-antispoof.mysql.sql" );
+		$dbw->sourceFile( "$IP/extensions/AntiSpoof/sql/mysql/tables-generated.sql" );
 		$dbw->sourceFile( "$IP/extensions/Babel/sql/tables-generated.sql" );
 		$dbw->sourceFile( "$IP/extensions/CheckUser/cu_changes.sql" );
 		$dbw->sourceFile( "$IP/extensions/CheckUser/cu_log.sql" );
