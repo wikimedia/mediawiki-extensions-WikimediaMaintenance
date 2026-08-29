0b14ebdd624f3eafe12a64b97a7e4da1be5ef760
Alexander Vorwerk <zabe@avorwerk.net>
2026-08-29 12:19:05 +0200

    Add script to fix fr_metadata drifts
    Bug: T420341
    Change-Id: Icb21aa3245b4ef31cd8a9839baba4cd8fa584d05

---
+++ maintenance/fixFileRevisionMetadataDrift.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * @license GPL-2.0-or-later
+ * @file
+ */
+
+// @codeCoverageIgnoreStart
+require_once __DIR__ . '/WikimediaMaintenance.php';
+// @codeCoverageIgnoreEnd
+
+use MediaWiki\Maintenance\Maintenance;
+use Wikimedia\Rdbms\SelectQueryBuilder;
+
+/**
+ * Maintenance script to fix drifts in fr_metadata field for old images.
+ */
+class FixFileRevisionMetadataDrift extends Maintenance {
+
+	public function __construct() {
+		parent::__construct();
+		$this->addDescription( 'Script to fix drifts in fr_metadata fields' );
+		$this->setBatchSize( 1000 );
+		$this->addOption( 'start', 'Name of file to start with', false, true );
+		$this->addOption( 'end', 'Name of file to end with', false, true );
+		$this->addOption( 'dry-run', 'Only show what would be changed without making updates' );
+		$this->addOption(
+			'sleep',
+			'Time to sleep between each batch (in seconds). Default: 0',
+			false,
+			true
+		);
+	}
+
+	public function execute() {
+		$verbose = $this->hasOption( 'verbose' );
+		$dryRun = $this->hasOption( 'dry-run' );
+		$start = $this->getOption( 'start', false );
+		$end = $this->getOption( 'end', false );
+		$sleep = (int)$this->getOption( 'sleep', 0 );
+
+		$dbw = $this->getPrimaryDB();
+		$batchSize = $this->getBatchSize();
+
+		$queryBuilderTemplate = $dbw->newSelectQueryBuilder()
+			->select( [
+				'oi_name',
+				'oi_timestamp',
+				'oi_sha1',
+				'oi_metadata',
+				'fr_id',
+				'fr_metadata',
+				'file_id'
+			] )
+			->from( 'oldimage' )
+			->join(
+				'file',
+				'f',
+				'f.file_name = oi_name'
+			)
+			->join(
+				'filerevision',
+				'fr',
+				[ 'fr.fr_file = f.file_id', 'fr.fr_timestamp = oi_timestamp', 'fr.fr_sha1 = oi_sha1' ]
+			);
+
+		if ( $end !== false ) {
+			$queryBuilderTemplate->andWhere( $dbw->expr( 'oi_name', '<=', $end ) );
+		}
+
+		$queryBuilderTemplate
+			->orderBy( 'oi_name', SelectQueryBuilder::SORT_ASC )
+			->orderBy( 'oi_timestamp', SelectQueryBuilder::SORT_ASC )
+			->limit( $batchSize );
+
+		$batchCondition = [];
+		if ( $start !== false ) {
+			$batchCondition[] = $dbw->expr( 'oi_name', '>=', $start );
+		}
+
+		$totalChecked = 0;
+		$totalFixed = 0;
+		$filesProcessed = 0;
+
+		// Fix drifts in oldimage vs filerevision
+		do {
+			$queryBuilder = clone $queryBuilderTemplate;
+			$res = $queryBuilder
+				->andWhere( $batchCondition )
+				->caller( __METHOD__ )
+				->fetchResultSet();
+
+			$batchFixed = 0;
+			foreach ( $res as $row ) {
+				$totalChecked++;
+
+				if ( $row->oi_metadata != $row->fr_metadata ) {
+					$batchFixed++;
+					$totalFixed++;
+
+					$this->output(
+						"MISMATCH: {$row->oi_name} @ {$row->oi_timestamp} - " .
+						"oi_metadata={$row->oi_metadata}, fr_metadata={$row->fr_metadata}"
+					);
+
+					if ( !$dryRun ) {
+						$dbw->newUpdateQueryBuilder()
+							->update( 'filerevision' )
+							->set( [ 'fr_metadata' => $row->oi_metadata ] )
+							->where( [ 'fr_id' => $row->fr_id ] )
+							->caller( __METHOD__ )->execute();
+
+						$this->output( " -> FIXED to {$row->oi_metadata}\n" );
+					} else {
+						$this->output( " -> WOULD FIX to {$row->oi_metadata}\n" );
+					}
+				} elseif ( $verbose ) {
+					$this->output(
+						"OK: {$row->oi_name} @ {$row->oi_timestamp} - " .
+						"oi_metadata={$row->oi_metadata}, fr_metadata={$row->fr_metadata}\n"
+					);
+				}
+			}
+
+			if ( $res->numRows() > 0 ) {
+				$lastRow = $res->current();
+				$res->seek( $res->numRows() - 1 );
+				$lastRow = $res->current();
+				$batchCondition = [ $dbw->expr( 'oi_name', '>', $lastRow->oi_name ) ];
+				$filesProcessed++;
+			}
+
+			if ( $batchFixed > 0 ) {
+				$this->output( "Batch completed: {$batchFixed} drifts fixed in this batch.\n" );
+			}
+
+			$this->waitForReplication();
+			if ( $sleep ) {
+				sleep( $sleep );
+			}
+
+		} while ( $res->numRows() === $batchSize );
+
+		// Fix drifts in image vs filerevision (current revisions)
+		$this->output( "\nChecking current image revisions...\n" );
+
+		$imageQueryBuilderTemplate = $dbw->newSelectQueryBuilder()
+			->select( [
+				'img_name',
+				'img_metadata',
+				'fr_id',
+				'fr_metadata',
+				'file_id'
+			] )
+			->from( 'image' )
+			->join(
+				'file',
+				'f',
+				'f.file_name = img_name'
+			)
+			->join(
+				'filerevision',
+				'fr',
+				[ 'fr.fr_file = f.file_id', 'fr.fr_timestamp = img_timestamp', 'fr.fr_sha1 = img_sha1' ]
+			);
+
+		if ( $end !== false ) {
+			$imageQueryBuilderTemplate->andWhere( $dbw->expr( 'img_name', '<=', $end ) );
+		}
+
+		$imageQueryBuilderTemplate
+			->orderBy( 'img_name', SelectQueryBuilder::SORT_ASC )
+			->limit( $batchSize );
+
+		$imageBatchCondition = [];
+		if ( $start !== false ) {
+			$imageBatchCondition[] = $dbw->expr( 'img_name', '>=', $start );
+		}
+
+		$imagesProcessed = 0;
+		do {
+			$imageQueryBuilder = clone $imageQueryBuilderTemplate;
+			$imageRes = $imageQueryBuilder
+				->andWhere( $imageBatchCondition )
+				->caller( __METHOD__ )
+				->fetchResultSet();
+
+			$batchFixed = 0;
+			foreach ( $imageRes as $row ) {
+				$totalChecked++;
+
+				if ( $row->img_metadata != $row->fr_metadata ) {
+					$batchFixed++;
+					$totalFixed++;
+
+					$this->output(
+						"MISMATCH (current): {$row->img_name} - " .
+						"img_metadata={$row->img_metadata}, fr_metadata={$row->fr_metadata}"
+					);
+
+					if ( !$dryRun ) {
+						$dbw->newUpdateQueryBuilder()
+							->update( 'filerevision' )
+							->set( [ 'fr_metadata' => $row->img_metadata ] )
+							->where( [ 'fr_id' => $row->fr_id ] )
+							->caller( __METHOD__ )->execute();
+
+						$this->output( " -> FIXED to {$row->img_metadata}\n" );
+					} else {
+						$this->output( " -> WOULD FIX to {$row->img_metadata}\n" );
+					}
+				} elseif ( $verbose ) {
+					$this->output(
+						"OK (current): {$row->img_name} - " .
+						"img_metadata={$row->img_metadata}, fr_metadata={$row->fr_metadata}\n"
+					);
+				}
+			}
+
+			if ( $imageRes->numRows() > 0 ) {
+				$lastRow = $imageRes->current();
+				$imageRes->seek( $imageRes->numRows() - 1 );
+				$lastRow = $imageRes->current();
+				$imageBatchCondition = [ $dbw->expr( 'img_name', '>', $lastRow->img_name ) ];
+				$imagesProcessed++;
+			}
+
+			if ( $batchFixed > 0 ) {
+				$this->output( "Batch completed: {$batchFixed} drifts fixed in this batch.\n" );
+			}
+
+			$this->waitForReplication();
+			if ( $sleep ) {
+				sleep( $sleep );
+			}
+
+		} while ( $imageRes->numRows() === $batchSize );
+
+		$this->output( "\nSummary:\n" );
+		$this->output( "- Total oldimage revisions checked: {$totalChecked}\n" );
+		$this->output( "- Total drifts found: {$totalFixed}\n" );
+		$this->output( "- Oldimage revisions processed: {$filesProcessed}\n" );
+		$this->output( "- Current image revisions processed: {$imagesProcessed}\n" );
+	}
+}
+
+// @codeCoverageIgnoreStart
+$maintClass = FixFileRevisionMetadataDrift::class;
+require_once RUN_MAINTENANCE_IF_MAIN;
+// @codeCoverageIgnoreEnd
